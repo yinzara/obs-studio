b4e2435845eb2526f5c280347bdd8270eb9fb766
Matt Morrissette <matt@goji.com>
2017-10-30 22:50:22 -0700

    cmake: adding Qt5WebSocketsd and QtNetworkd
    added Qt5WebSocketsd and QtNetworkd as required DLLs with OBS studio

--- cmake/Modules/CopyMSVCBins.cmake
+++ cmake/Modules/CopyMSVCBins.cmake
@@ -142,6 +142,8 @@ if (CMAKE_CONFIGURATION_TYPES MATCHES "Debug")
 		"${Qt5Core_DIR}/../../../bin/Qt5Cored.dll"
 		"${Qt5Core_DIR}/../../../bin/Qt5Guid.dll"
 		"${Qt5Core_DIR}/../../../bin/Qt5Widgetsd.dll"
+		"${Qt5Core_DIR}/../../../bin/Qt5WebSocketsd.dll"
+		"${Qt5Core_DIR}/../../../bin/Qt5Networkd.dll"
 		"${Qt5Core_DIR}/../../../bin/libGLESv2d.dll"
 		"${Qt5Core_DIR}/../../../bin/libEGLd.dll")
 	file(GLOB QT_DEBUG_PLAT_BIN_FILES
@@ -153,6 +155,8 @@ if (CMAKE_CONFIGURATION_TYPES MATCHES "Rel")
 		"${Qt5Core_DIR}/../../../bin/Qt5Core.dll"
 		"${Qt5Core_DIR}/../../../bin/Qt5Gui.dll"
 		"${Qt5Core_DIR}/../../../bin/Qt5Widgets.dll"
+		"${Qt5Core_DIR}/../../../bin/Qt5WebSockets.dll"
+		"${Qt5Core_DIR}/../../../bin/Qt5Network.dll"
 		"${Qt5Core_DIR}/../../../bin/libGLESv2.dll"
 		"${Qt5Core_DIR}/../../../bin/libEGL.dll")
 	file(GLOB QT_PLAT_BIN_FILES
